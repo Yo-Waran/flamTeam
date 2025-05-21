f7092371c6cd84b5ec14a89e4859e4f5e20c67b8
Ram Yogeshwaran <ramyogeshwaran.yogi@gmail.com>
2025-05-21 13:12:47 +0530

    Added by git commit

---
+++ app_dev/test.js
@@ -0,0 +1,15 @@
+class Test {
+  constructor() {
+    this.name = 'class name Test';
+  }
+
+  run() {
+    console.log('Running test...');
+  }
+}
+
+const test = new Test();
+test.run();
+console.log(test.name);
+// This is a test file for the Test class
+
